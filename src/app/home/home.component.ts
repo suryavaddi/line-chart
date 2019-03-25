43b45780794f68db68253295472b766a2d082ecc
sasi_vaddi <suryasasankvaddi@gmail.com>
2019-03-25 11:30:37 -0400

    Real time line chart assignment for Huan

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-home",
@@ -8,8 +8,16 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
   data: any;
   options: any;
+  liveData: any;
   pointBackgroundColors: any[] = [];
-  pointRadius:  any[] = [];
+  pointRadius: any[] = [];
+  livePointBackgroundColors: any[] = [];
+  livePointRadius: any[] = [];
+
+  xData: any[] = [];
+  yData: any[] = [];
+  unlimited: any = 1;
+  @ViewChild("chart") chart: ElementRef<HTMLInputElement>;
 
   constructor() {
     this.data = {
@@ -61,7 +69,21 @@ export class HomeComponent implements OnInit {
           fill: false,
           borderColor: "#b3b300",
           pointBackgroundColor: this.pointBackgroundColors,
-          pointRadius:this.pointRadius
+          pointRadius: this.pointRadius
+        }
+      ]
+    };
+
+    this.liveData = {
+      labels: this.xData,
+      datasets: [
+        {
+          label: "Time vs Data",
+          data: this.yData,
+          fill: false,
+          borderColor: "#b3b300",
+          pointBackgroundColor: this.livePointBackgroundColors,
+          pointRadius: this.pointRadius
         }
       ]
     };
@@ -70,17 +92,70 @@ export class HomeComponent implements OnInit {
       if (data > 0.1) {
         this.pointBackgroundColors.push("#ffcc00");
         this.pointRadius.push(10);
-      } 
-      else if(data < -0.1) {
+      } else if (data < -0.1) {
         this.pointBackgroundColors.push("#ffcc00");
         this.pointRadius.push(10);
-      } 
-      else {
+      } else {
         this.pointBackgroundColors.push("#b3b300");
         this.pointRadius.push(1);
       }
     });
+
+    /* this.liveData.datasets[0].data.forEach(data => {
+      if (data > 5) {
+        this.livePointBackgroundColors.push("red");
+        this.livePointRadius.push(10);
+      }  else {
+        this.livePointBackgroundColors.push("green");
+        this.livePointRadius.push(1);
+      }
+    }); */
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateChart();
+  }
+
+  generateChart() {
+    console.log("chart generating");
+    for (let i = 0; i < this.unlimited; i++) {
+      console.log("for loop");
+      var x = new Date().getMilliseconds()/100;
+      var y = Math.floor(Math.random() * 10);
+      if (this.xData.length >= 20) {
+        this.xData.shift();
+      }
+      if (this.yData.length >= 20) {
+        this.yData.shift();
+      }
+      if (this.livePointBackgroundColors.length >= 20) {
+        this.livePointBackgroundColors.shift();
+      }
+      if (this.livePointRadius.length >= 20) {
+        this.livePointRadius.shift();
+      }
+      if (y >5) {
+        this.livePointBackgroundColors.push("yellow");
+        this.livePointRadius.push(10);
+      }  else {
+        this.livePointBackgroundColors.push("red");
+        this.livePointRadius.push(1);
+      }
+
+      this.xData.push(x);
+      this.yData.push(y);
+      
+      console.log(this.xData);
+      console.log(this.yData);
+     // this.liveData.refresh();
+      /* this.xData = xData;
+      this.yData = yData; */
+      (<any>this.chart).refresh()
+    }
+    setTimeout(() => {
+      this.unlimited++;
+
+      this.generateChart();
+    }, 2000);
+  }
 }
